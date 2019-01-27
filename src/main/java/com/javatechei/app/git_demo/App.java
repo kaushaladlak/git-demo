1a758c8cce996545ad840bc2932a19866c4eabef
kaushaladlak <adlakkaushal@gmail.com>
2019-01-27 10:53:08 +0530

    added git-demo project to local repo

---
+++ src/main/java/com/javatechei/app/git_demo/App.java
@@ -0,0 +1,16 @@
+package com.javatechei.app.git_demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	 public void adddata(String data){
+		 System.out.println("data="+ data);
+	 }
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
