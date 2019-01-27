93f166d2652d26e7fdff7bcd3ccf3b9a3152bd0d
kaushaladlak <adlakkaushal@gmail.com>
2019-01-27 11:26:54 +0530

    added if condition to check the data

--- src/main/java/com/javatechei/app/git_demo/App.java
+++ src/main/java/com/javatechei/app/git_demo/App.java
@@ -4,13 +4,15 @@ package com.javatechei.app.git_demo;
  * Hello world!
  *
  */
-public class App 
-{
-	 public void adddata(String data){
-		 System.out.println("data="+ data);
-	 }
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public void adddata(String data) {
+		System.out.println("data=" + data);
+		if (data.equals("java techei")) {
+			System.out.println("well come");
+		}
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
 }
